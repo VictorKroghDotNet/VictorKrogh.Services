5e1789d64abf6894708b8f9bc3b92dec7704a0d2
KingoBoiii <kingodk@outlook.dk>
2024-03-18 21:21:36 +0100

    Add project files.

---
+++ VictorKrogh.Services/ServiceBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace VictorKrogh.Services;
+
+public class ServiceBase<TService>(ILogger<TService> logger)
+    where TService : class, IService
+{
+    protected ILogger<TService> Logger { get; } = logger;
+}
